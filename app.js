4a48fad772aef84383624a662c224b49f0e2f2bb
miaccc <ta828383@gmail.com>
2022-06-19 19:23:58 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+// routes setting
+app.get('/',(req, res) => {
+  res.render('index')
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+
+})
